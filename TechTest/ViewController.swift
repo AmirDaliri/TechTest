ae6a034ad2e22f8782e3bb22377bad38d10d63d7
idaliri <idaliri69@gmail.com>
2019-07-19 21:08:52 +0300

    initial

---
+++ TechTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechTest
+//
+//  Created by Amir Daliri on 19.07.2019.
+//  Copyright © 2019 AmirDaliri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
