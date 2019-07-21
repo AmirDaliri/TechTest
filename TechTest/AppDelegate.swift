c25f557552289063322a534f7aa069bd183a121f
idaliri <idaliri69@gmail.com>
2019-07-22 02:38:13 +0300

    add loading screen with check health

--- TechTest/AppDelegate.swift
+++ TechTest/AppDelegate.swift
@@ -17,10 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        window = showFeedController()
+        window = showLaunchScreenController()
         return true
     }
 
+    func showLaunchScreenController() -> PCWindow {
+        return PCWindow(rootViewController: LaunchScreenViewController())
+    }
+    
     func showFeedController() -> PCWindow {
         return PCWindow(rootViewController: PCNavigationController(rootViewController: MainViewController()))
     }
